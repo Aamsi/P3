569c90d013747d6f4cbb6228556ee630ab2323cc
Aamsi <ismaelouali45@gmail.com>
2019-10-15 19:29:26 +0200

    Maze with random pos for item

--- bisMaze.py
+++ bisMaze.py
@@ -1,45 +1,49 @@
+from random import randrange
+from maze import maze
+
 class Character:
-    #Character : 
-    #   - MacGyver (position and inventory)
-    #   - Guardian (position and inventory = 0)
+    """Character : 
+       - MacGyver (position and inventory)
+       - Guardian (position and inventory = 0)"""
     
-    #Two attributes: position and inventory
+    """Two attributes: position and inventory"""
     def __init__(self, position_x, position_y, inventory):
         self.position_x = position_x
         self.position_y = position_y
         self.inventory = inventory
 
 class Item:
-    #Item:
-    #   - Niddle
-    #   - Tube
-    #   - Ether
-    #Each have one attribute: position
+    """#Item:
+       - Niddle
+       - Tube
+       - Ether
+       Each have one attribute: position"""
 
-    def __init__(self, nom, position_x, position_y, position = []):
+    def __init__(self, nom, position=[]):
         self.nom = nom
-        self.position_x = position_x
-        self.position_y = position_y
-        self.position = [self.position_x, self.position_y]
+        self.position = position
 
 
-#check if the coordonnates of MG == coordonnates of the path on x
 def path(macGyver, maze, initial_position):
+    """check if the coordonnates of MG == coordonnates of the path on x"""
+
     try:
         if maze[macGyver.position_y][macGyver.position_x] == '0' or macGyver.position_x == -1 or macGyver.position_y == -1:
             macGyver.position_x = initial_position[0]
             macGyver.position_y = initial_position[1]
             print("Vous ne pouvez pas aller là")
+
     except IndexError:
         print("Vous ne pouvez pas allez là")
         macGyver.position_x = initial_position[0]
         macGyver.position_y = initial_position[1]
     
-#ask input to the user
 def ask_input():
+    """ask input to the user"""
+
     can_move = False
 
-    while can_move == False:
+    while not can_move:
         move = input("Right: d\nLeft: q\nUp: z\nDown: s\n")
         
         if move != "s" and move != "d" and move != "z" and move != "q":
@@ -50,8 +54,9 @@ def ask_input():
     
     return move
     
-#movement method: ask the user to press z, q, s or d to move and return macGyver's position += 1
 def movement(macGyver):
+    """movement method: ask the user to press z, q, s or d to move and return macGyver's position += 1"""
+
     position = [macGyver.position_x, macGyver.position_y]
     move = ask_input()
        
@@ -70,14 +75,16 @@ def movement(macGyver):
    
     return position
 
-#If Mac Gyver position == Object position, add 1 to MacGyver inventory
 def get_object(macGyver, Item): 
+    """If Mac Gyver position == Object position, add 1 to MacGyver inventory"""
+
     macGyver.inventory += 1
     picked_up(Item)
     return macGyver.inventory
 
-#Check if MacGyver's in front of the guardian:
 def check_guardian(macGyver, guardian):
+    """Check if MacGyver's in front of the guardian:"""
+
     finished = False
     x_finished = check_x_position(macGyver, guardian)
     y_finished = check_y_position(macGyver, guardian)
@@ -94,9 +101,9 @@ def win_lose(macGyver, guardian):
     inventory = macGyver.inventory
     
     win = False
-    if finished == True and inventory == 3:
+    if finished and inventory == 3:
         win = True
-    elif finished == True and inventory != 3:
+    elif finished and inventory != 3:
         win = False
         print("Vous n'avez pas assez d'objets pour tuer le guardien!")
 
@@ -124,3 +131,23 @@ def check_y_position(macGyver, guardian):
 
 def picked_up(Item):
     Item.position = [-100, -100]
+
+def random_pos(maze):
+    x = 0
+    pos = []
+    pos_rand = [0, 0]
+
+    while x < 15:
+        y = 0
+
+        while y < 15:
+            if maze[y][x] == "1":
+                pos_rand = [x, y]
+                pos.append(pos_rand)
+            y += 1
+        x += 1
+
+    i = randrange(len(pos))
+    coord = pos[i]
+
+    return coord
--- mainMaze.py
+++ mainMaze.py
@@ -1,55 +1,63 @@
-from bisMaze import Character, Item, movement, get_object, check_guardian, win_lose, path
+from bisMaze import (Character, Item, movement, get_object, check_guardian,
+                    win_lose, path, random_pos)                   
 from maze import maze
 
-#I create all my Items:
+"""I create all my Items:"""
 
-#Character have a position and an inventory
+"""Character have a position and an inventory"""
 macGyver = Character(5, 0, 0)
 guardian = Character(5, 14, 0)
 
-#Item just have a position
-niddle = Item("Aiguille", 4, 10)
-tube = Item("Tube en plastique", 7, 6)
-ether = Item("Ether", 8, 9)
+"""Item just have a position"""
+niddle = Item("Aiguille", random_pos(maze))
+tube = Item("Tube en plastique", random_pos(maze))
+ether = Item("Ether", random_pos(maze))
+print("Aiguille", niddle.position)
+print("Tube", tube.position)
+print("Ether", ether.position)
 
-#I put my Item in a list so I can iterate whenever MacGyver moves to check if an object is here
+"""I put my Item in a list so I can iterate whenever MacGyver moves to check if
+   an object is here"""
 Items = [niddle, tube, ether]
 
-#I create two var for position and inventory so it's clearer
+"""I create two var for position and inventory so it's clearer"""
 position = [macGyver.position_x, macGyver.position_y]
 player_inventory = macGyver.inventory
 
 finished = False
-#Ask the user to move if he's not in front of the guardian 
-while finished == False:
 
-    #We register initial coordonnates to apply in path parameter
+"""Ask the user to move if he's not in front of the guardian""" 
+while not finished:
+
+    """We register initial coordonnates to apply in path parameter"""
     position_init = [macGyver.position_x, macGyver.position_y]
 
-    #We call the method movement to add 1 to MacGyver's inventory
+    """We call the method movement to add 1 to MacGyver's inventory"""
     position = movement(macGyver)
-    print("Position ", position)
+    print("Position avant vérif", position)
 
-    #Check if he's on the wall or not, if he can move, i += 1
+    """Check if he's on the wall"""
     path(macGyver, maze, position_init)
     
     print("Coordonnées après vérif", macGyver.position_x, macGyver.position_y)
     
-    #Once he moved, check if there's an object and/or if he is at the end, if not, movement()
+    """"Once he moved, check if there's an object and/or if he is at the end,
+        if not, movement()"""
     for objet in Items:
 
-        #If there's an object, add 1 to the inventory
+        """"If there's an object, add 1 to the inventory"""
         if position == objet.position:
             player_inventory = get_object(macGyver, objet)
             print("Vous avez rammassé: {}".format(objet.nom))
-            print("Vous avez {} objet(s) dans votre inventaire".format(player_inventory))
+            print("Vous avez {} objet(s) dans votre inventaire"
+                  .format(player_inventory))
 
-    #check if he's in front of the guardian
+    """check if he's in front of the guardian"""
     finished = check_guardian(macGyver, guardian)
 
-#check if he won after finished is True
+"""check if he won after finished is True"""
 win = win_lose(macGyver, guardian)
-if win == True:
+if win:
     print("Bravo! Vous avez gagné")
 else:
     print("Désolé, vous avez perdu!")
