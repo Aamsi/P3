e0bb8ef4da423f821dc44315618f1b28ca044b78
Aamsi <ismaelouali45@gmail.com>
2019-11-04 01:14:46 +0100

    Ajustements

--- Readme.md
+++ Readme.md
@@ -1,8 +1,7 @@
 Le programme principal se trouve dans main.py
-Les classes et les fonctions sont dans core.py
-level0.txt est le labyrinthe.
+Les classes et les fonctions sont dans core.py et level0.txt est le labyrinthe.
 
-Pour se déplacer, utiliser z, q, s et d.
+Pour se déplacer, utiliser les flèches.
 Pour quitter le jeu avant la fin, appuyez sur ESC.
 
 Pour installer les librairies nécessaires: pip install -r requirements.txt
--- core.py
+++ core.py
@@ -2,56 +2,50 @@ from random import randrange
 
 import pygame as pg
 
+
 class Character:
-    """Character : 
+    """Character :
        - MacGyver (position and inventory)
        - Guardian (position and inventory = 0)"""
-    
+
     """Three attributes: position_x, position_y and inventory"""
     def __init__(self, position_x, position_y, inventory):
-        self.position_x = position_x
-        self.position_y = position_y
+        self.position = [position_x, position_y]
         self.inventory = inventory
-    
-    def movement(self):
-        """Modify hero position after asking input"""
 
-        position = [self.position_x, self.position_y]
+    def movement(self, maze):
+        """Modify hero position after asking input"""
+        initial_position = [self.position[0], self.position[1]]
 
         move = input_pg()
-        
-        if move == 'move right':
-            self.position_x += 1
-            position[0] = self.position_x
 
-        elif move == 'move left':
-            self.position_x -= 1
-            position[0] = self.position_x
-            
-        elif move == 'move up':
-            self.position_y -= 1
-            position[1] = self.position_y
+        if move == 1:
+            self.position[0] += 1
 
-        elif move == 'move down':
-            self.position_y += 1
-            position[1] = self.position_y
-        
-        elif move == 'quit':
+        elif move == 0:
+            self.position[0] -= 1
+
+        elif move == 2:
+            self.position[1] -= 1
+
+        elif move == 3:
+            self.position[1] += 1
+
+        elif move == 4:
             pg.quit()
 
-        return position
-    
-    def path(self, maze, initial_position):
+        self.check_path(maze, initial_position)
+
+    def check_path(self, maze, initial_position):
         """Check if he can move toward the new position"""
 
         try:
-            if maze[self.position_y][self.position_x] == 0 or self.position_x == -1 or self.position_y == -1:
-                self.position_x = initial_position[0]
-                self.position_y = initial_position[1]
+            if maze[self.position[1]][self.position[0]] == 0 or \
+               self.position[0] == -1 or self.position[1] == -1:
+                self.position = initial_position
 
         except IndexError:
-            self.position_x = initial_position[0]
-            self.position_y = initial_position[1]
+            self.position = initial_position
 
     def get_object(self, item):
         """If hero on an item, add 1 to inventory"""
@@ -59,18 +53,14 @@ class Character:
         self.inventory += 1
         item.picked_up()
         return self.inventory
-    
+
     def check_guardian(self, other):
         """Check if MacGyver's in front of the guardian:"""
 
         finished = False
-        x_finished = self.check_x_position(other)
-        y_finished = self.check_y_position(other)
 
-        if x_finished and y_finished:
+        if self.position == other.position:
             finished = True
-        else:
-            finished = False
 
         return finished
 
@@ -78,39 +68,9 @@ class Character:
         """Check if MacGyver fills all conditions to win"""
 
         finished = self.check_guardian(other)
-        inventory = self.inventory
-    
-        win = False
-        if finished and inventory == 3:
-            win = True
-        elif finished and inventory != 3:
-            win = False
 
-        return win
-    
-    def check_x_position(self, other):
-        """Check x position"""
+        return finished and self.inventory == 3
 
-        x_finished = False
-
-        if self.position_x == other.position_x:
-            x_finished = True
-        else:
-            x_finished = False
-        
-        return x_finished
-    
-    def check_y_position(self, other):
-        """Check y position"""
-
-        y_finished = False
-
-        if self.position_y == other.position_y:
-            y_finished = True
-        else:
-            y_finished = False
-        
-        return y_finished
 
 class Item:
     """#Item:
@@ -119,25 +79,48 @@ class Item:
        - Ether
        Each have two attributes: name and position"""
 
-    def __init__(self, nom, position=[]):
+    def __init__(self, nom):
         self.nom = nom
-        self.position = position
-        self.position_x = position[0]
-        self.position_y = position[1]
-    
+        self.position = []
+
+    def random_pos(self, maze):
+        """Generate a random position for item"""
+
+        x = 0
+        pos = []
+        pos_rand = [0, 0]
+
+        while x < 15:
+            y = 1
+
+            while y < 14:
+                if maze[y][x] == 1:
+                    pos_rand = [x, y]
+                    pos.append(pos_rand)
+                y += 1
+            x += 1
+
+        i = randrange(len(pos))
+        coord = pos[i]
+
+        self.position = [coord[0], coord[1]]
+
     def picked_up(self):
         """Modify item position when picked up"""
 
-        self.position_x = -100
-        self.position_y = -100
-        self.position = [-100, -100]
+        self.position[0] = -100
+        self.position[1] = -100
+
 
 class Maze:
+    """Maze has two attributes:
+        - Level
+        - Its structure """
 
     def __init__(self, level):
         self.level = level
-        self.laby = []
-    
+        self.struct = self.loadmaze()
+
     def loadmaze(self):
         """Load the level from the text file"""
 
@@ -156,10 +139,10 @@ class Maze:
                 maze.append(case_list)
 
         self.laby = maze
-        
+
         return maze
-    
-    def maze_interface(self, ecran):
+
+    def draw_maze(self, ecran):
         """Display the maze, given the level"""
 
         wall = pg.image.load('ressource/tile_wall.png').convert_alpha()
@@ -173,90 +156,106 @@ class Maze:
                     ecran.blit(tile_wall, (x, y))
                 x += 40
             y += 40
-    
-    def random_pos(self):
-        """Generate a random position for item"""
+
+    def start(self):
+        """Find the starting position for MacGyver's position"""
 
         x = 0
-        pos = []
-        pos_rand = [0, 0]
+        maze_start = []
 
         while x < 15:
-            y = 1
+            y = 0
 
-            while y < 14:
-                if self.laby[y][x] == 1:
-                    pos_rand = [x, y]
-                    pos.append(pos_rand)
+            while y < 15:
+                if self.struct[y][x] == 2:
+                    maze_start = [x, y]
                 y += 1
             x += 1
 
-        i = randrange(len(pos))
-        coord = pos[i]
+        return maze_start
 
-        return coord
+    def end(self):
+        """Find the ending position for the Guardian's position"""
+
+        x = 0
+        maze_end = []
+
+        while x < 15:
+            y = 0
+
+            while y < 15:
+                if self.struct[y][x] == 3:
+                    maze_end = [x, y]
+                y += 1
+            x += 1
+
+        return maze_end
 
 
 def input_pg():
     """Input for the user to move MacGyver or quit
        Used in class method movement()"""
+    MOVE_LEFT = 0
+    MOVE_RIGHT = 1
+    MOVE_UP = 2
+    MOVE_DOWN = 3
+    QUIT = 4
 
     move = None
 
     for event in pg.event.get():
         if event.type == pg.KEYDOWN:
-            if event.key == pg.K_d:
-                move = 'move right'
+            if event.key == pg.K_RIGHT:
+                move = MOVE_RIGHT
 
-            elif event.key == pg.K_a:
-                move = 'move left'
-                
-            elif event.key == pg.K_w:
-                move = 'move up'
-                
-            elif event.key == pg.K_s:
-                move = 'move down'
+            elif event.key == pg.K_LEFT:
+                move = MOVE_LEFT
+
+            elif event.key == pg.K_UP:
+                move = MOVE_UP
+
+            elif event.key == pg.K_DOWN:
+                move = MOVE_DOWN
 
             elif event.key == pg.K_ESCAPE:
-                move = 'quit'
-    
+                move = QUIT
+
     return move
 
-def sprite_interaction(sprite, sprite_file, ecran):
-    """Display all objects that interacts with the user"""
+
+def load_image(sprite_file):
+    """Load every images"""
 
     sprite_image = pg.image.load(sprite_file).convert_alpha()
-    sprite_sized = pg.transform.scale(sprite_image, (40, 40))
+    sprite_scaled = pg.transform.scale(sprite_image, (40, 40))
 
-    x = sprite.position_x * 40
-    y = sprite.position_y * 40
+    return sprite_scaled
 
-    ecran.blit(sprite_sized, (x, y))
+
+def draw_sprite(instance, sprite, ecran):
+    """Display all instances that interacts with the user"""
+
+    x = instance.position[0] * 40
+    y = instance.position[1] * 40
+
+    ecran.blit(sprite, (x, y))
+
 
 def inventory_counter_sprite(hero, screen):
     """Counts MacGyver's inventory and display number"""
 
-    counter_0 = pg.image.load('ressource/compteur_0.png').convert_alpha()
-    counter0_sized = pg.transform.scale(counter_0, (40, 40))
+    counters_file = ['ressource/compteur_0.png', 'ressource/compteur_1.png',
+                     'ressource/compteur_2.png', 'ressource/compteur_3.png']
+    counters = []
 
-    counter_1 = pg.image.load('ressource/compteur_1.png').convert_alpha()
-    counter1_sized = pg.transform.scale(counter_1, (40, 40))
+    for counter_file in counters_file:
+        counter = pg.image.load(counter_file).convert_alpha()
+        counter_sized = pg.transform.scale(counter, (40, 40))
+        counters.append(counter_sized)
 
-    counter_2 = pg.image.load('ressource/compteur_2.png').convert_alpha()
-    counter2_sized = pg.transform.scale(counter_2, (40, 40))
+    screen.blit(counters[hero.inventory], (540, 0))
 
-    counter_3 = pg.image.load('ressource/compteur_3.png').convert_alpha()
-    counter3_sized = pg.transform.scale(counter_3, (40, 40))
 
-    if hero.inventory == 0:
-        screen.blit(counter0_sized, (540, 0))
-    elif hero.inventory == 1:
-        screen.blit(counter1_sized, (540, 0))
-    elif hero.inventory == 2:
-        screen.blit(counter2_sized, (540, 0))
-    elif hero.inventory == 3:
-        screen.blit(counter3_sized, (540, 0))
-    
 def win_sprite(screen, win):
     """Display if won or lost"""
 
@@ -270,4 +269,3 @@ def win_sprite(screen, win):
         screen.blit(win_sized, (100, 200))
     else:
         screen.blit(loose_sized, (100, 200))
-    
